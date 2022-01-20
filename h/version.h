c0deee2e986535a909f3f461a705233c21b61f34
Gregg Young <ygk@qwest.net>
2022-01-20 13:05:00 -0800

    Update version.h for 3.11.GKY release.

--- h/version.h
+++ h/version.h
@@ -9,9 +9,9 @@
 
 #else
 #define VER_MAJOR 3
-#define VER_MINOR 9
+#define VER_MINOR 11
 // #define VER_REVISION ""   // Must be string with no spaces - leading dot optional
-#define VER_REVISION ".1-shl"   // Must be string with no spaces - leading dot optional
+#define VER_REVISION "GKY"   // Must be string with no spaces - leading dot optional
 
 #endif
 
